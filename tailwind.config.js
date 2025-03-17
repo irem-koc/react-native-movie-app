f37e268af979359be410b093543e0f53e577f8dd
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-03-17 22:01:43 +0300

    Add Babel and Metro configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#2A1A40",
+        light: {
+          100: "#F4F2FF",
+          200: "#DCCFFF",
+          300: "#B6A6FF",
+        },
+        dark: {
+          100: "#1E0B33",
+          200: "#150829",
+        },
+        accent: "#AB8BFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
