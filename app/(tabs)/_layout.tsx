392a396f1f3641d5a302e6d223a8d85a3ec45769
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-03-18 21:00:22 +0300

    Add tab bar icons and styling to the index screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,30 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, Text } from "react-native";
 
 const Layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Index", headerShown: false }}
+        options={{
+          title: "Index",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground source={images.highlight}>
+                <Image
+                  source={icons.home}
+                  tintColor={"#151322"}
+                  className="size-5"
+                />
+                <Text>Home</Text>
+              </ImageBackground>
+            </>
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
