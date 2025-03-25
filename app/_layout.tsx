33ab8789047ac3251ec4f956832cb4c9294f3ecb
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-03-26 01:23:49 +0300

    Add QueryClient to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import "./global.css";
-
+const queryClient = new QueryClient();
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
