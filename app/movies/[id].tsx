39b09fa441eeeb9613a5aa7ff42e105a228e8fa9
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-03-29 23:17:15 +0300

    Refactor MovieDetails component to display loading and error states

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,16 +1,49 @@
 import { useApiGetMovieDetails } from "@/hooks/api";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const { data } = useApiGetMovieDetails(id);
-  console.log(data, "datadatadata");
+  const { data, isLoading, error } = useApiGetMovieDetails(id);
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 bg-primary justify-center items-center">
+        <ActivityIndicator size="large" color="#AB8BFF" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className="flex-1 bg-primary justify-center items-center">
+        <Text className="text-light-100 text-lg">
+          Failed to load movie details.
+        </Text>
+      </View>
+    );
+  }
+
   return (
-    <View className="flex-1 bg-primary">
-      <Text>MovieDetails</Text>
-    </View>
+    <ScrollView className="flex-1 bg-primary p-4">
+      {data?.poster_path && (
+        <Image
+          source={{ uri: `https://image.tmdb.org/t/p/w500${data.poster_path}` }}
+          className="w-full h-96 rounded-lg mb-4"
+        />
+      )}
+      <Text className="text-light-100 text-2xl font-bold mb-2">
+        {data?.title}
+      </Text>
+      <Text className="text-light-200 text-lg mb-4">{data?.overview}</Text>
+      <Text className="text-light-300 text-base">
+        Release Date: {data?.release_date}
+      </Text>
+      <Text className="text-light-300 text-base">
+        Rating: {data?.vote_average} / 10
+      </Text>
+    </ScrollView>
   );
 };
 
